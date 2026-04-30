178f9a501065c7327f21b20273326f58fe171445
haithampm <Haitham.pm@gmail.com>
2026-04-30 20:17:51 +0300

    Mount project owner task panel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ImportModeSelector from "@/components/ImportModeSelector";
 import ImplementationActivitiesMatrix from "@/components/ImplementationActivitiesMatrix";
+import ProjectOwnerTaskPanel from "@/components/projects/ProjectOwnerTaskPanel";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")!).render(
     <App />
     <ImportModeSelector />
     <ImplementationActivitiesMatrix />
+    <ProjectOwnerTaskPanel />
   </>,
 );
