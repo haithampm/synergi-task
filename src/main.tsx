0aaf9681130131370945495617c5337551d52743
haithampm <Haitham.pm@gmail.com>
2026-04-30 16:45:15 +0300

    Mount live project lifecycle matrix globally

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ImportModeSelector from "@/components/ImportModeSelector";
 import ImplementationActivitiesMatrix from "@/components/ImplementationActivitiesMatrix";
+import DynamicProjectLifecycleMatrix from "@/components/projects/DynamicProjectLifecycleMatrix";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")!).render(
     <App />
     <ImportModeSelector />
     <ImplementationActivitiesMatrix />
+    <DynamicProjectLifecycleMatrix />
   </>,
 );
