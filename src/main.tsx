2b80a5b136ac73f791f0c5d90b23f1316492648f
haithampm <Haitham.pm@gmail.com>
2026-04-30 15:13:01 +0300

    Mount implementation activities matrix

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ImportModeSelector from "@/components/ImportModeSelector";
+import ImplementationActivitiesMatrix from "@/components/ImplementationActivitiesMatrix";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <ImportModeSelector />
+    <ImplementationActivitiesMatrix />
   </>,
 );
