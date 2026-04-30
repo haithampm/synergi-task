4e639d13f73f3e592fc7c13b19d5ef5005dab6e3
haithampm <Haitham.pm@gmail.com>
2026-04-30 13:52:31 +0300

    Mount import mode selector

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import ImportModeSelector from "@/components/ImportModeSelector";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <ImportModeSelector />
+  </>,
+);
