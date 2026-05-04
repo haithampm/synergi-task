34c84eb979152a7f3af86d98d9e36a604d46d3d7
haithampm <Haitham.pm@gmail.com>
2026-05-04 09:05:04 +0300

    Enable project schedule importer on master schedule

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App.tsx";
 import ImplementationActivitiesMatrix from "@/components/ImplementationActivitiesMatrix";
 import ProjectOwnerTaskPanel from "@/components/projects/ProjectOwnerTaskPanel";
 import ProjectTaskBulkActionsPanel from "@/components/projects/ProjectTaskBulkActionsPanel";
+import ProjectScheduleImporter from "@/components/schedule/ProjectScheduleImporter";
 import PMODeliveryCyclePanel from "@/components/PMODeliveryCyclePanel";
 import "./index.css";
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <ImplementationActivitiesMatrix />
     <ProjectOwnerTaskPanel />
     <ProjectTaskBulkActionsPanel />
+    <ProjectScheduleImporter />
     <PMODeliveryCyclePanel />
   </>,
 );
