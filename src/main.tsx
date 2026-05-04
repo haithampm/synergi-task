9d282b76cb839640392aaa51e2d33654fb179d30
haithampm <Haitham.pm@gmail.com>
2026-05-04 09:26:23 +0300

    Fix app crash by rendering workspace panels inside provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import ImplementationActivitiesMatrix from "@/components/ImplementationActivitiesMatrix";
-import ProjectOwnerTaskPanel from "@/components/projects/ProjectOwnerTaskPanel";
-import ProjectTaskBulkActionsPanel from "@/components/projects/ProjectTaskBulkActionsPanel";
-import ProjectScheduleImporter from "@/components/schedule/ProjectScheduleImporter";
-import PMODeliveryCyclePanel from "@/components/PMODeliveryCyclePanel";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <ImplementationActivitiesMatrix />
-    <ProjectOwnerTaskPanel />
-    <ProjectTaskBulkActionsPanel />
-    <ProjectScheduleImporter />
-    <PMODeliveryCyclePanel />
-  </>,
-);
+createRoot(document.getElementById("root")!).render(<App />);
