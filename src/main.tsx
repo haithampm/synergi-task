8a3dc72316a1aea2dc4f9639438586549d464ef1
haithampm <Haitham.pm@gmail.com>
2026-05-10 13:57:20 +0300

    Install Supabase egress guard at app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
+import { installSupabaseEgressGuard } from "./integrations/supabase/egress-guard";
 import App from "./App.tsx";
 import "./index.css";
+
+installSupabaseEgressGuard();
 
 createRoot(document.getElementById("root")!).render(<App />);
